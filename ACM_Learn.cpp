303d9a00ac6012d8bde6fc56630f9121235accc9
cq01 <chq_github@outlook.com>
2019-10-29 19:32:56 +0800

    1552 A+B for Input-Output Practice (I)

--- ACM_Learn.cpp
+++ ACM_Learn.cpp
@@ -1,6 +1,11 @@
-﻿#include <iostream>
-
+﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
+#include <iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a, b;
+	while (cin >> a >> b)
+	{
+		cout << a + b << endl;
+	}
 }
