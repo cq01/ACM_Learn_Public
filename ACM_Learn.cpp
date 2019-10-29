b06308773d3d76acd956f6e94be89f01d043202f
cq01 <chq_github@outlook.com>
2019-10-29 19:34:09 +0800

    1553 A+B for Input-Output Practice (II)

--- ACM_Learn.cpp
+++ ACM_Learn.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 int main()
 {
-	int a, b;
-	while (cin >> a >> b)
+	int a, b, n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
+		cin >> a >> b;
 		cout << a + b << endl;
 	}
 }
