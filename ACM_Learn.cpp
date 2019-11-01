88746a67d6a62ee0c326b8348d31b0a1aa35cb83
cq01 <chq_github@outlook.com>
2019-11-01 13:43:28 +0800

    1562: The 3n + 1 problem

--- ACM_Learn.cpp
+++ ACM_Learn.cpp
@@ -1,20 +1,56 @@
-﻿//Copyright by cq01, 2019 Licensed under the MIT license : http://www.opensource.org/licenses/mit-license.php
+﻿//from https://blog.csdn.net/metaphysis/article/details/6431937
+//Licensed under CC 4.0 BY-SA(http://creativecommons.org/licenses/by-sa/4.0/)
 #include <iostream>
-#include<cstring>
+
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+#define MAXSIZE 100000
 using namespace std;
-char str[1001];
+
+int cache[MAXSIZE] = { 0 };
+long int times(long int n);
 int main()
 {
-	int n;
-	cin >> n;
-	cin.get();
-	for (int i = 0; i < n; ++i)
+	int i, j;
+	int res = 0;
+	int step;
+	while (cin >> i >> j)
 	{
-		cin.getline(str, 1000);
-		cout << str << endl << endl;
+		int start = min(i, j);
+		int end = max(i, j);
+		for (long int k = start; k <= end; ++k)
+		{
+			if ((step = times(k)) > res)
+			{
+				res = step;
+			}
+		}
+		cout << i << ' ' << j << ' ' << res << endl;
+		res = 0;
 	}
-	while (cin >> str)
+}
+
+long int times(long int n)
+{
+	if (n == 1)
 	{
-		cout << str << endl << endl;
+		return 1;
 	}
+	if (n & 1)//n%2
+	{
+		n += (n << 1) + 1;//3n+1
+	}
+	else
+	{
+		n >>= 1;//n/2
+	}
+	if (n < MAXSIZE)//search cache
+	{
+		if (!cache[n])
+		{
+			cache[n] = times(n);
+		}
+		return cache[n] + 1;
+	}
+	return times(n) + 1;
 }
